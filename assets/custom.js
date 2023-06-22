51683986104329d049fb19a47c357720a9600812
zakaria bouzangad <bouzangadzakaria@iMac-de-Sigr.local>
2023-06-22 15:17:37 +0100

    wrokign on gift cart fucntionnality part 2

--- assets/custom.js
+++ assets/custom.js
@@ -16,38 +16,38 @@
     'theme:cart:change',
     function (event) {
       //gift functionnaly
-      if(window.location.pathname=='/cart'){
-        if(!document.querySelector('[data-gift-item]')){
-          const elements = document.querySelectorAll('.add-btn-text')
-          for (let i = 0; i < elements.length; i++) {
-            elements[i].classList.remove('disabled');
-            elements[i].classList.remove('isTheCurrentProduct');
-            elements[i].innerHTML='Add to cart';
-          }
-          if(document.querySelector('.btn-wrap')){
-            document.querySelector('.btn-wrap').innerHTML="";
-          }
-        }
-  
-        //if only item in the cart is gift item i should clear the cart
-        if(event.detail.cartCount==1 && document.querySelector('[data-gift-item]')){
-          $.ajax({
-            type: 'POST',
-            url: '/cart/clear.js',
-            dataType: 'json',
-            success: function(cart){location.reload();},
-            error: function(err){location.reload();}
-          });
-        }
-      }
-      //delete gift from cart when the press is less than item
       if(window.cart.subtotal<=5000){
         document
         .querySelector("[data-gift-item]")
         ?.querySelector(".cart__item__remove")
         .click();
       }
-      console.log(event)
+      else if(event.detail.cartCount==1 && document.querySelector('[data-gift-item]')){
+        $.ajax({
+          type: 'POST',
+          url: '/cart/clear.js',
+          dataType: 'json',
+          success: function(cart){location.reload();},
+          error: function(err){location.reload();}
+        });
+      }
+      else if(window.location.pathname=='/cart'){
+        
+        //we should update the cart now
+          fetch(theme.routes.cart_url)
+          .then(this.cartErrorsHandler)
+          .then((response) => response.text())
+          .then((response) => {
+            const element = document.createElement('div');
+            element.innerHTML = response;
+
+            const cleanResponse = element.querySelector("div[content-gift-product]");
+            if(cleanResponse){
+              document.querySelector("div[content-gift-product]").innerHTML=cleanResponse.innerHTML;
+            }
+          })
+          .catch((error) => console.log(error));
+      }
   })
   
 
