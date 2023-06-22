bff64a3bfddc22c286d8406d15076f5e500d2f72
zakaria bouzangad <bouzangadzakaria@iMac-de-Sigr.local>
2023-06-22 09:20:29 +0100

    Revert "begin working on cart gift functionnality"
    This reverts commit 0494a667a16784aeff5c06164121642bd141edd5.

--- assets/custom.js
+++ assets/custom.js
@@ -12,43 +12,6 @@
   // Add custom code below this line
 
 
-  document.addEventListener(
-    'theme:cart:change',
-    function (event) {
-      //gift functionnaly
-      if(window.location.pathname=='/cart'){
-        if(!document.querySelector('[data-gift-item]')){
-          const elements = document.querySelectorAll('.add-btn-text')
-          for (let i = 0; i < elements.length; i++) {
-            elements[i].classList.remove('disabled');
-            elements[i].classList.remove('isTheCurrentProduct');
-            elements[i].innerHTML='Add to cart';
-          }
-          if(document.querySelector('.btn-wrap')){
-            document.querySelector('.btn-wrap').innerHTML="";
-          }
-        }
-  
-        //if only item in the cart is gift item i should clear the cart
-        if(event.detail.cartCount==1 && document.querySelector('[data-gift-item]')){
-          $.ajax({
-            type: 'POST',
-            url: '/cart/clear.js',
-            dataType: 'json',
-            success: function(cart){location.reload();},
-            error: function(err){location.reload();}
-          });
-        }
-      }
-      //delete gift from cart when the press is less than item
-      if(window.cart.subtotal<=5000){
-        document
-        .querySelector("[data-gift-item]")
-        ?.querySelector(".cart__item__remove")
-        .click();
-      }
-      console.log(event)
-  })
   
 
 
