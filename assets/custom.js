08b2cf5bd47b7ae9125ef36994de7b3d52440426
bouzangad zakaria <bouzangadzakaria@gmail.com>
2023-06-24 14:08:52 +0100

    finaliazing free gift functionnality

--- assets/custom.js
+++ assets/custom.js
@@ -11,29 +11,18 @@
 (function() {
   // Add custom code below this line
 
+  if(window.theme.enable_freegift){
+    document.addEventListener(
+      'theme:cart:change',
+      function (event) {
+        //gift functionnaly
+        if(window.cart.subtotal<window.theme.free_gift_limit){
+          document
+          .querySelector("[data-gift-item]")
+          ?.querySelector(".cart__item__remove")
+          .click();
 
-  document.addEventListener(
-    'theme:cart:change',
-    function (event) {
-      //gift functionnaly
-      if(window.cart.subtotal<=5000){
-        document
-        .querySelector("[data-gift-item]")
-        ?.querySelector(".cart__item__remove")
-        .click();
-      }
-      else if(event.detail.cartCount==1 && document.querySelector('[data-gift-item]')){
-        $.ajax({
-          type: 'POST',
-          url: '/cart/clear.js',
-          dataType: 'json',
-          success: function(cart){location.reload();},
-          error: function(err){location.reload();}
-        });
-      }
-      else if(window.location.pathname=='/cart'){
-        
-        //we should update the cart now
+          //we should update the cart now
           fetch(theme.routes.cart_url)
           .then(this.cartErrorsHandler)
           .then((response) => response.text())
@@ -47,13 +36,35 @@
             }
           })
           .catch((error) => console.log(error));
-      }
-  })
-  
+        }
+        else if(event.detail.cartCount==1 && document.querySelector('[data-gift-item]')){
+          $.ajax({
+            type: 'POST',
+            url: '/cart/clear.js',
+            dataType: 'json',
+            success: function(cart){location.reload();},
+            error: function(err){location.reload();}
+          });
+        }
+        else if(window.location.pathname=='/cart'){
+          
+          //we should update the cart now
+            fetch(theme.routes.cart_url)
+            .then(this.cartErrorsHandler)
+            .then((response) => response.text())
+            .then((response) => {
+              const element = document.createElement('div');
+              element.innerHTML = response;
 
-
-
-
+              const cleanResponse = element.querySelector("div[content-gift-product]");
+              if(cleanResponse){
+                document.querySelector("div[content-gift-product]").innerHTML=cleanResponse.innerHTML;
+              }
+            })
+            .catch((error) => console.log(error));
+        }
+    })
+  }
   // ^^ Keep your scripts inside this IIFE function call to 
   // avoid leaking your variables into the global scope.
 })();
